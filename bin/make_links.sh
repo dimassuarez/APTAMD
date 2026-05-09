a0da72371dab46b3b7df25826d68eee59cb0fbad
Dimas Suárez Rodríguez <108862116+dimassuarez@users.noreply.github.com>
2026-05-09 17:54:21 +0200

    Update make_links.sh

--- bin/make_links.sh
+++ bin/make_links.sh
@@ -1,10 +1,16 @@
-#/bin/bash
-# Doing Links (if not done)
+#!/bin/bash
+if [ -z "$APTAMD" ]; then echo "APTAMD variable is not defined!" ; exit; fi
+# Fix permissions
+cd $APTAMD
+chmod u=rwx,g=rx,o=rx $(find . -name "*.sh")
+# Bin directory
+cd $APTAMD/bin
+# Do links
 DO_LIST=$(find ../ -name "do_*.sh")
 for file in ${DO_LIST}
 do
     lkfile=$(basename $file)
-    lkfile=${lkfile/.sh/} 
+    lkfile=${lkfile/.sh/}
     echo $file $lkfile
     if [ ! -e $lkfile ]
     then
